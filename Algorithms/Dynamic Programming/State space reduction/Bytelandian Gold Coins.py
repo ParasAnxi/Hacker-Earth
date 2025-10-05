43c461c2aa1e68b7f1f69824bcffc0c0cc8e7e25
Paras Bhandari <paras.bhandari7010@gmail.com>
2025-10-05 09:41:01 +0530

    c

---
+++ Algorithms/Dynamic Programming/State space reduction/Bytelandian Gold Coins.py
@@ -0,0 +1,17 @@
+# Write your code here
+dp = {0: 0}
+
+def exchange(x, dp):
+    if x in dp:
+        return dp[x]
+    else:
+        dp[x] = max(x, exchange(x // 2, dp) + exchange(x // 3, dp) + exchange(x // 4, dp))
+        return dp[x]
+ 
+ 
+while True:
+    try:
+        n = int(input())
+        print(exchange(n, dp))
+    except:
+        break
