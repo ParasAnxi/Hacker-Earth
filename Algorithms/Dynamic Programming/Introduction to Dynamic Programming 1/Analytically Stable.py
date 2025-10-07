7960a8d69a73ef7f2959d98752024b789627e778
Paras Bhandari <paras.bhandari7010@gmail.com>
2025-10-07 13:58:05 +0530

    c

---
+++ Algorithms/Dynamic Programming/Introduction to Dynamic Programming 1/Analytically Stable.py
@@ -0,0 +1,22 @@
+MOD = 1000000007
+def solve(N):
+    # Write your code here
+    count = [0] * 10
+    curr = [0] * 10
+    res = 0
+    for i in map(int, N):
+        if i > 0:
+            res += curr[i]
+            res %= MOD
+            curr[i] += (count[i - 1] * (count[i - 1] - 1)) // 2
+            curr[i] %= MOD
+        count[i] += 1
+    return res
+ 
+ 
+T = int(input())
+for _ in range(T):
+    N = input()
+ 
+    out_ = solve(N)
+    print(out_)
